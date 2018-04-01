53206df572e2fa0f69e8d882854c6239be7d2f3c
SaharHaltzi <34007523+SaharHaltzi@users.noreply.github.com>
2018-04-01 16:03:10 +0300

    question 2

---
+++ B18 Ex01 Abraham 305758880 Sahar 308235407/B18_Ex01_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace B18_Ex01_2
+{
+    public class Program
+    {
+        //string.Format("The numbers in decimal format are:\n{0}\n{1}\n{2}", decimalNumber1, decimalNumber2, decimalNumber3));
+        static void Main()
+        {
+            const string fiveStars = "*****";
+            const string threeStars = "***";
+            const string oneStar = "*";
+            string newline= Environment.NewLine;
+
+            Console.WriteLine(string.Format("{1}{0} {2} {0}  {3}  {0}  {3}  {0} {2} {0}{1}",newline,fiveStars,threeStars,oneStar));
+            Console.WriteLine("Press any key to continue...");
+            String sahar =Console.ReadLine();
+        }
+    }
+}
