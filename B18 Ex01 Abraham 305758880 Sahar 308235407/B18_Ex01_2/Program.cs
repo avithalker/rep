b4e9277c0a8b482fdc7df5e64e19a93f2260a4c4
SaharHaltzi <34007523+SaharHaltzi@users.noreply.github.com>
2018-04-02 14:11:49 +0300

    question3

--- B18 Ex01 Abraham 305758880 Sahar 308235407/B18_Ex01_2/Program.cs
+++ B18 Ex01 Abraham 305758880 Sahar 308235407/B18_Ex01_2/Program.cs
@@ -5,7 +5,6 @@ namespace B18_Ex01_2
 {
     public class Program
     {
-        //string.Format("The numbers in decimal format are:\n{0}\n{1}\n{2}", decimalNumber1, decimalNumber2, decimalNumber3));
         static void Main()
         {
             const string fiveStars = "*****";
@@ -15,7 +14,7 @@ namespace B18_Ex01_2
 
             Console.WriteLine(string.Format("{1}{0} {2} {0}  {3}  {0}  {3}  {0} {2} {0}{1}",newline,fiveStars,threeStars,oneStar));
             Console.WriteLine("Press any key to continue...");
-            String sahar =Console.ReadLine();
+            String input =Console.ReadLine();
         }
     }
 }
---
+++ B18 Ex01 Abraham 305758880 Sahar 308235407/B18_Ex01_3/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+
+namespace B18_Ex01_3
+{
+    public class Program
+    {
+        
+
+        static void Main()
+        {
+            Console.WriteLine("Please enter your desired hourglass height:\n");
+            int hourglassHeight = int.Parse(Console.ReadLine());
+            printHourglass(hourglassHeight);
+
+            Console.WriteLine("Press any key to continue...");
+            String input = Console.ReadLine();
+        }
+
+        private static string setStars(int i_numOfStars)
+        {
+            string stars = "";
+
+            for (int i = 0; i < i_numOfStars;i++ )
+            {
+                stars += "*";         
+            }
+
+            return stars;
+        }
+
+        private static string setSpaces(int i_numOfSpaces)
+        {
+            string spaces = "";
+            for(int i=0;i<i_numOfSpaces;i++)
+            {
+                spaces += " ";
+            }
+            return spaces;
+        }
+
+        public static void  printHourglass(int i_hourglassHeight)
+        {
+            string newLine=Environment.NewLine;
+            string hourglass = "";
+
+            for(int i=i_hourglassHeight;i>0;i-=2)
+            {
+                hourglass+=setSpaces((i_hourglassHeight - i) / 2);
+                hourglass+=setStars(i);
+                hourglass+=setSpaces((i_hourglassHeight - i) / 2);
+                hourglass += newLine;
+                
+            }
+            for(int i=1;i<=i_hourglassHeight;i+=2)
+            {
+                hourglass += setSpaces((i_hourglassHeight - i) / 2);
+                hourglass += setStars(i);
+                hourglass += setSpaces((i_hourglassHeight - i) / 2);
+                hourglass += newLine;
+            }
+
+            Console.WriteLine(hourglass);
+        }
+       
+
+           
+    }
+}
+
