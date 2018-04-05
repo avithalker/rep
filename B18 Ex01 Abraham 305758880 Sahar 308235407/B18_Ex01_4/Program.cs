4fecd4429944c1c4aeb90e55c2d57603b8973d19
SaharHaltzi <34007523+SaharHaltzi@users.noreply.github.com>
2018-04-05 13:57:45 +0300

    stylecop errors

--- B18 Ex01 Abraham 305758880 Sahar 308235407/B18_Ex01_4/Program.cs
+++ B18 Ex01 Abraham 305758880 Sahar 308235407/B18_Ex01_4/Program.cs
@@ -6,12 +6,13 @@ namespace B18_Ex01_4
     {
         static void Main()
         {
-            string inputString =getStringFromUser();
-            while(!isStringValid(inputString))
+            string inputString = getStringFromUser();
+            while (!isStringValid(inputString))
             {
-                inputString=getStringFromUser();
+                inputString = getStringFromUser();
             }
-            if(isStringPalindrome(inputString))
+
+            if (isStringPalindrome(inputString))
             {
                 Console.WriteLine("The string is a palindrome");
             }
@@ -19,13 +20,14 @@ namespace B18_Ex01_4
             {
                 Console.WriteLine("The string is not a palindrome");
             }
-            if(isALetter(inputString[0]))
+
+            if (isALetter(inputString[0]))
             {
-                Console.WriteLine(string.Format("The string has {0} lowercase letters",getNumberOfLowercaseLetters(inputString)));
+                Console.WriteLine(string.Format("The string has {0} lowercase letters", getNumberOfLowercaseLetters(inputString)));
             }
             else
             {
-                if(isNumberEven(inputString))
+                if (isNumberEven(inputString))
                 {
                     Console.WriteLine("The string is an Even number");
                 }
@@ -33,24 +35,24 @@ namespace B18_Ex01_4
                 {
                     Console.WriteLine("The string is an Odd number");
                 }
-            }
-
-               
-            string sahar = Console.ReadLine();
-          
+            }               
+            ////Console.ReadLine();
         }
+
         private static string getStringFromUser()
         {
             Console.WriteLine("Please Enter 8-character string which contains only numbers or only letters :\n ");
-            string inputString=Console.ReadLine();
+            string inputString = Console.ReadLine();
 
             return inputString;
         }
+
         private static bool isALetter(char o_char)
         {
-            return ((o_char >= 'a' && o_char <= 'z') || (o_char >= 'A' && o_char <= 'Z'));
+            return (o_char >= 'a' && o_char <= 'z') || (o_char >= 'A' && o_char <= 'Z');
         }
-        private static bool isStringValid( string i_userInputString)
+
+        private static bool isStringValid(string i_userInputString)
         {
             if (i_userInputString[0] >= '0' && i_userInputString[0] <= '9')
             {
@@ -60,14 +62,13 @@ namespace B18_Ex01_4
                     {
                         return false;
                     }
-                }
-                
+                }                
             }
             else if (isALetter(i_userInputString[0]))
             {
-                for(int i=1; i<i_userInputString.Length;i++)
+                for (int i = 1; i < i_userInputString.Length; i++)
                 {
-                    if(!isALetter(i_userInputString[i]))
+                    if (!isALetter(i_userInputString[i]))
                     {
                         return false;
                     }
@@ -80,43 +81,41 @@ namespace B18_Ex01_4
 
             return true;
         }
+
         private static bool isStringPalindrome(string i_userInputString)
         {
-            int reverseIndex=i_userInputString.Length-1;
-            for (int i = 0; i < i_userInputString.Length;i++ )
+            int reverseIndex = i_userInputString.Length - 1;
+            for (int i = 0; i < i_userInputString.Length; i++)
             {
-                    if(i_userInputString[i]!= i_userInputString[reverseIndex])
+                    if (i_userInputString[i] != i_userInputString[reverseIndex])
                     {
                         return false;
                     }
+
                     reverseIndex--;
             }
+
                 return true;
         }
+
         private static bool isNumberEven(string i_userInputString)
         {
             int inputNumber = int.Parse(i_userInputString);
-            return (inputNumber % 2==0);
+            return inputNumber % 2 == 0;
         }
+
         private static int getNumberOfLowercaseLetters(string i_userInputString)
         {
             int numberOfLowercaseLetters = 0;
-            for(int i=0;i<i_userInputString.Length;i++)
+            for (int i = 0; i < i_userInputString.Length; i++)
             {
-                if(i_userInputString[i]>='a' || i_userInputString[i]<='z')
+                if (i_userInputString[i] >= 'a' || i_userInputString[i] <= 'z')
                 {
                     numberOfLowercaseLetters++;
                 }
-
             }
 
             return numberOfLowercaseLetters;
-        }
-        
-           
-        }
-
-     
-        
+        }           
+        }        
     }
-
