a5867e607bf855b916fe0eca6321f2b4f3475199
avithalker <30372420+avithalker@users.noreply.github.com>
2018-04-30 00:00:38 +0300

    game configuration class was added

---
+++ B18 Ex02 Abraham 305758880 Sahar 308235407/BusinessLogic/Configuration/GameConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLogic.Enums;
+using System.Collections.Generic;
+
+namespace BusinessLogic.Configuration
+{
+    public class GameConfiguration
+    {
+        private int m_BoardSize;
+        private GameModes.eGameModes m_GameMode;
+        private List<PlayerConfiguration> m_PlayerConfigurations;
+
+        public GameModes.eGameModes GameMode
+        {
+            get { return m_GameMode; }
+            set
+            {
+                m_GameMode = value;
+                if(m_GameMode == GameModes.eGameModes.OnePlayerGame)
+                {
+                    PlayerConfiguration computerPlayerConfig = new PlayerConfiguration();
+
+                    computerPlayerConfig.PlayerName = "Computer";
+                    computerPlayerConfig.PlayerType = PlayerTypes.ePlayerTypes.Computer;
+                    AddPlayerConfiguration(computerPlayerConfig);
+                }
+            }
+        }
+
+        public int BoardSize
+        {
+            get { return m_BoardSize; }
+            set { m_BoardSize = value; }
+        }
+
+        public GameConfiguration()
+        {
+            m_PlayerConfigurations = new List<PlayerConfiguration>(2);
+        }
+
+        public void AddPlayerConfiguration(PlayerConfiguration i_PlayerConfiguration)
+        {
+            m_PlayerConfigurations.Add(i_PlayerConfiguration);
+        }
+
+    }
+}
---
+++ B18 Ex02 Abraham 305758880 Sahar 308235407/BusinessLogic/Enums/PlayerTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace BusinessLogic.Enums
+{
+    public class PlayerTypes
+    {
+        public enum ePlayerTypes
+        {
+            Human = 1,
+            Computer = 2
+        }
+    }
+}
