50429f018efe9303fe8b88d1554445b3b30515dd
avithalker <30372420+avithalker@users.noreply.github.com>
2018-04-30 00:21:54 +0300

    application manager initialize

--- B18 Ex02 Abraham 305758880 Sahar 308235407/CheckersApp/ApplicationManager.cs
+++ B18 Ex02 Abraham 305758880 Sahar 308235407/CheckersApp/ApplicationManager.cs
@@ -1,6 +1,39 @@
-﻿namespace CheckersApp
+﻿using BusinessLogic.Configuration;
+using BusinessLogic.Enums;
+
+namespace CheckersApp
 {
     public class ApplicationManager
     {
+        
+        public void InitializeGame()
+        {
+            int currentPlayer = 1;
+            GameConfiguration gameConfiguration = new GameConfiguration();
+            PlayerConfiguration mainPlayerConfiguration = new PlayerConfiguration();
+
+            mainPlayerConfiguration.PlayerName = UIManager.GetPlayerName(currentPlayer);
+            mainPlayerConfiguration.PlayerType = PlayerTypes.ePlayerTypes.Human;
+            gameConfiguration.AddPlayerConfiguration(mainPlayerConfiguration);
+            gameConfiguration.BoardSize = UIManager.GetBoardSize();
+            gameConfiguration.GameMode = UIManager.GetGameMode();
+            if(gameConfiguration.GameMode ==GameModes.eGameModes.TwoPlayersGame)
+            {
+                PlayerConfiguration secondaryPlayerConfiguration = new PlayerConfiguration();
+
+                currentPlayer++;
+                secondaryPlayerConfiguration.PlayerName = UIManager.GetPlayerName(currentPlayer);
+                secondaryPlayerConfiguration.PlayerType = PlayerTypes.ePlayerTypes.Human;
+                gameConfiguration.AddPlayerConfiguration(secondaryPlayerConfiguration);
+            }
+
+            //todo: initialize the game manager here with the game configuration
+
+        }
+
+        public void StartGame()
+        {
+
+        }
     }
 }
--- B18 Ex02 Abraham 305758880 Sahar 308235407/CheckersApp/Program.cs
+++ B18 Ex02 Abraham 305758880 Sahar 308235407/CheckersApp/Program.cs
@@ -4,6 +4,15 @@
     {
         public static void Main(string[] args)
         {
+            StartGame();
+        }
+
+        private static void StartGame()
+        {
+            ApplicationManager appManager = new ApplicationManager();
+
+            appManager.InitializeGame();
+            appManager.StartGame();
         }
     }
 }
