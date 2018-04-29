2ac85bc3131e7fc099c553be41442e6f2b8dd476
SaharHaltzi <sahar.haltzi@gmail.com>
2018-04-29 19:42:04 +0300

    create BusinessLogic dll
    add Cell class and BoardManager class

---
+++ B18 Ex02 Abraham 305758880 Sahar 308235407/AbstractPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AbstractPlayer
+{
+	public class AbstractPlayer
+	{
+        int m_points;
+        int m_name;
+        int m_numOfTools;
+
+        public GetPoints()
+        {
+            return m_points;
+        }
+        public SetPoints(int points)
+
+	}
+}
---
+++ B18 Ex02 Abraham 305758880 Sahar 308235407/BoardGame.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BoardManager
+{
+    public class BoardManager
+    {
+
+    }
+}
