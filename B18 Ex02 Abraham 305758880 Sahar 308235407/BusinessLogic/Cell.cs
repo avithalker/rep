2f2475646d3d3494c1c0c89394939890a365b278
SaharHaltzi <sahar.haltzi@gmail.com>
2018-04-30 12:51:31 +0300

    organization
    organization

--- B18 Ex02 Abraham 305758880 Sahar 308235407/BusinessLogic/Cell.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BusinessLogic
-{
-    public class Cell
-    {
-    }
-
-    
-}
---
+++ B18 Ex02 Abraham 305758880 Sahar 308235407/BusinessLogic/Configuraton/PlayerConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLogic.Enums;
+
+namespace BusinessLogic.Configuration
+{
+    public class PlayerConfiguration
+    {
+        private string m_PlayerName;
+        private PlayerTypes.ePlayerTypes m_PlayerType;
+
+        public PlayerTypes.ePlayerTypes PlayerType
+        {
+            get { return m_PlayerType; }
+            set { m_PlayerType = value; }
+        }
+
+        public string PlayerName
+        {
+            get { return m_PlayerName; }
+            set { m_PlayerName = value; }
+        }
+    }
+}
