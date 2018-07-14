d2930422377fbb1cc4d25cd9c2f835bdd2274c7c
avithalker <30372420+avithalker@users.noreply.github.com>
2018-07-14 20:36:13 +0300

    first commit

---
+++ C18 Ex01 Abraham 305758880 Sahar 308235407/C18 Ex01 Abraham 305758880 Sahar 308235407/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C18_Ex01_Abraham_305758880_Sahar_308235407
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new UserProfileForm());
+        }
+    }
+}
---
+++ C18 Ex01 Abraham 305758880 Sahar 308235407/C18 Ex01 Abraham 305758880 Sahar 308235407/UserProfileForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace C18_Ex01_Abraham_305758880_Sahar_308235407
+{
+    partial class UserProfileForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // UserProfileForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Name = "UserProfileForm";
+            this.Text = "User profile";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
