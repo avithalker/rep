bf631c7f73629c96574ba5f2cf89f93c8bb7afbb
avi <avithalker@gmail.com>
2018-07-29 23:48:14 +0300

    clicking on the titledPictureBox bug was fixed

--- C18 Ex01 Abraham 305758880 Sahar 308235407/C18 Ex01 Abraham 305758880 Sahar 308235407/Components/UserControls/TitledPictureControl.cs
+++ C18 Ex01 Abraham 305758880 Sahar 308235407/C18 Ex01 Abraham 305758880 Sahar 308235407/Components/UserControls/TitledPictureControl.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace DesktopFacebook.Components.UserControls
 {
@@ -15,12 +16,30 @@ namespace DesktopFacebook.Components.UserControls
         public TitledPictureControl(string i_ImageUrl, string i_Title, string i_Id)
         {
             InitializeComponent();
+            WireAllControls(this);
             m_Id = i_Id;
             TitleLabel.Text = i_Title;
             if (!string.IsNullOrEmpty(i_ImageUrl))
             {
                 PhotoPictureBox.LoadAsync(i_ImageUrl);
             }
+        }
+
+        private void WireAllControls(Control i_MainLayoutControl)
+        {
+            foreach (Control childControl in i_MainLayoutControl.Controls)
+            {
+                childControl.Click += ctl_Click;
+                if (childControl.HasChildren)
+                {
+                    WireAllControls(childControl);
+                }
+            }
+        }
+
+        private void ctl_Click(object sender, EventArgs e)
+        {
+            this.InvokeOnClick(this, EventArgs.Empty);
         }
     }
 }
