ffb749e487142818bb7e18eae4d675a03cc40957
SaharHaltzi <sahar.haltzi@gmail.com>
2018-07-29 23:14:56 +0300

    FriendshipMatch feature
    FriendshipMatch feature

---
+++ C18 Ex01 Abraham 305758880 Sahar 308235407/C18 Ex01 Abraham 305758880 Sahar 308235407/Components/Pages/CheckFriendsMatchScale.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace DesktopFacebook.Components.Pages
+{
+    partial class CheckFriendsMatchScale
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.FriendsListBox = new System.Windows.Forms.CheckedListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.FriendshipMatchValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(422, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "choose a friend to check your match scale:";
+            // 
+            // FriendsListBox
+            // 
+            this.FriendsListBox.FormattingEnabled = true;
+            this.FriendsListBox.Location = new System.Drawing.Point(66, 92);
+            this.FriendsListBox.Name = "FriendsListBox";
+            this.FriendsListBox.Size = new System.Drawing.Size(417, 342);
+            this.FriendsListBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(74, 488);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(303, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "your friendship match scale is:";
+            // 
+            // FriendshipMatchValue
+            // 
+            this.FriendshipMatchValue.AutoSize = true;
+            this.FriendshipMatchValue.Location = new System.Drawing.Point(413, 488);
+            this.FriendshipMatchValue.Name = "FriendshipMatchValue";
+            this.FriendshipMatchValue.Size = new System.Drawing.Size(0, 25);
+            this.FriendshipMatchValue.TabIndex = 3;
+            // 
+            // CheckFriendsMatchScale
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.FriendshipMatchValue);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.FriendsListBox);
+            this.Controls.Add(this.label1);
+            this.Name = "CheckFriendsMatchScale";
+            this.Size = new System.Drawing.Size(1198, 836);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckedListBox FriendsListBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label FriendshipMatchValue;
+    }
+}
---
+++ C18 Ex01 Abraham 305758880 Sahar 308235407/C18 Ex01 Abraham 305758880 Sahar 308235407/CustomFeatures/FriendshipMatchScale/FriendshipMatchScaleCalculator.cs
@@ -0,0 +1,122 @@
+﻿using FacebookWrapper.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopFacebook.CustomFeatures.FriendshipMatchScale
+{
+    public class FriendshipMatchScaleCalculator
+    {
+        private int m_matchScaleValue;
+        private User m_loginUser;
+        private User m_usersFriend;
+
+        public FriendshipMatchScaleCalculator(User i_loginUser, User i_usersFriend)
+        {
+            m_loginUser = i_loginUser;
+            m_usersFriend = i_usersFriend;
+        }
+
+        public int CalculateMusicMatchPercentValue()
+        {
+            List<Page> MusicPageListOfLoginUser = getUsersMusicPageLIst(m_loginUser);
+            List<Page> MusicPageListOfUsersFriend = getUsersMusicPageLIst(m_usersFriend);
+            int numOfMatches = 0;
+            
+            foreach(Page page in MusicPageListOfUsersFriend)
+            {
+                foreach(Page loginUserpage in MusicPageListOfLoginUser)
+                {
+                    if(page.Id == loginUserpage.Id)
+                    {
+                        numOfMatches++;
+                    }
+                }
+            }
+
+            return (numOfMatches / MusicPageListOfLoginUser.Count) * 100 ;
+        }
+
+        private List<Page> getUsersMusicPageLIst(User i_user)
+        {
+            List<Page> MusicPageList = new List<Page>();
+
+            foreach (Page page in i_user.LikedPages.Where(isMusicPage))
+            {
+                if(page.Category.ToString() == "Musician/Band")
+                {
+                    MusicPageList.Add(page);
+                }
+            }
+
+            return MusicPageList;
+        }
+
+        public int CalculateEntertainmentPlacesMatch()
+        {
+            int numOfMatches = 0;
+
+            foreach (Checkin checkin in m_usersFriend.Checkins)
+            {
+               foreach(Checkin friendsCheckin in m_loginUser.Checkins)
+                {
+                    if(checkin.Place.Category == friendsCheckin.Place.Category)
+                    {
+                        numOfMatches++;
+                    }
+                }
+            }
+
+            return (numOfMatches / m_usersFriend.Checkins.Count) * 100;
+        }
+
+        public int CalculatePrivateInfoMatch()
+        {
+            int numOfMatches = 0;
+
+            if(m_loginUser.Hometown == m_usersFriend.Hometown)
+            {
+                numOfMatches++;
+            }
+            if(m_loginUser.RelationshipStatus == m_usersFriend.RelationshipStatus)
+            {
+                numOfMatches++;
+            }
+            if(getUsersCurrentWork(m_loginUser).Position == getUsersCurrentWork(m_usersFriend).Position)
+            {
+                numOfMatches++;
+            }
+
+            return (numOfMatches / 3) * 100;
+        }
+
+        private WorkExperience getUsersCurrentWork(User i_user)
+        {
+            foreach(WorkExperience work in i_user.WorkExperiences)
+            {
+                if ((string.IsNullOrEmpty((work.EndDate.ToString()))))
+                {
+                    return work;
+                }
+            }
+
+            return null;
+        }
+
+        public int Calculate()
+        {
+            int entertainmentPercentMatch = CalculateEntertainmentPlacesMatch() * 1 / 3;
+            int privateInfoMatch = CalculatePrivateInfoMatch() * 1 / 3;
+            int musicMatch = CalculateMusicMatchPercentValue() * 1 / 3;
+
+            return (musicMatch + privateInfoMatch + entertainmentPercentMatch);
+        }
+
+        private bool isMusicPage(Page i_page)
+        {
+            return i_page.Category == "Musician/Band";
+        }
+    }
+}
