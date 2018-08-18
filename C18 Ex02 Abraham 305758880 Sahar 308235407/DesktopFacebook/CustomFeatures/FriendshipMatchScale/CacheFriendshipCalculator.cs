017d3ca273caf585fa03aefc345309674586e8fe
SaharHaltzi <sahar.haltzi@gmail.com>
2018-08-18 22:01:52 +0300

    cached Calculator
    cached Calculator

---
+++ C18 Ex02 Abraham 305758880 Sahar 308235407/DesktopFacebook/CustomFeatures/FriendshipMatchScale/CacheFriendshipCalculator.cs
@@ -0,0 +1,26 @@
+﻿using FacebookWrapper.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopFacebook.CustomFeatures.FriendshipMatchScale
+{
+    public class CacheFriendshipCalculator
+    {
+        public FriendshipMatchScaleCalculator m_FriendshipCalculator { get; set; }
+        //public List<String> m_CachedFriendsId { get; set; } 
+        public Dictionary<String, List<Checkin>> m_FriendsCheckins { get; set; }
+        public Dictionary<String, int> m_CachedFriendshipMatchValues { get; set; }
+        public List<Checkin> m_UsersCheckins;
+        public List<Page> m_MusicPages { get; set; }
+        public Dictionary<String, Object> m_UsersGeneralDetails {get; set;}
+
+      
+        public int Calculate()
+        {
+            return 0;
+        }
+    }
+}
