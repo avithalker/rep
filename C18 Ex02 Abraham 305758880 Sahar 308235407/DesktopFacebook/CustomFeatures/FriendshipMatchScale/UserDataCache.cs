b068a0c807fe8bc0964acb9c9d669076c3ed5e42
SaharHaltzi <sahar.haltzi@gmail.com>
2018-08-18 22:16:11 +0300

    cahce calculator - add cache class
    cahce calculator - add cache class

---
+++ C18 Ex02 Abraham 305758880 Sahar 308235407/DesktopFacebook/CustomFeatures/FriendshipMatchScale/UserDataCache.cs
@@ -0,0 +1,16 @@
+﻿using FacebookWrapper.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopFacebook.CustomFeatures.FriendshipMatchScale
+{
+    public class UserDataCache
+    {
+        public List<Checkin> m_UsersCheckins;
+        public List<Page> m_MusicPages { get; set; }
+        public Dictionary<String, Object> m_UsersGeneralDetails { get; set; }
+    }
+}
